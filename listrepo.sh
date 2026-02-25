e9e7a3209fec60eb3bbc2fd1aa5f16764d387814
Juiz <111427193+Raghav6698@users.noreply.github.com>
2026-02-25 13:30:47 +0530

    Update listrepo.sh

--- listrepo.sh
+++ listrepo.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-curl --silent --user "Philips Software" "https://github.com/philips-software" | npx jq '.[].ssh_url' | while read repo
-do
-    repo="${repo%\"}"
-    repo="${repo#\"}"
-    echo "$repo"
-done
+# curl --silent --user "Philips Software" "https://github.com/philips-software" | npx jq '.[].ssh_url' | while read repo
+# do
+#     repo="${repo%\"}"
+#     repo="${repo#\"}"
+#     echo "$repo"
+# done
+
+USERNAME="Philips Software"
+
+curl -s "https://api.github.com/users/$USERNAME/repos" \
+| jq -r '.[].name'
